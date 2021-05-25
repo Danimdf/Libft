3e9c56b24f913bb79cbea3658ac2b945fb82ea2d
Daniela Monteiro <danimdf360@gmail.com>
2021-05-25 01:06:57 -0300

    modificaçoes

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: dmonteir <dmonteir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/24 10:34:33 by dmonteir          #+#    #+#             */
-/*   Updated: 2021/05/25 00:21:13 by dmonteir         ###   ########.fr       */
+/*   Updated: 2021/05/25 01:06:26 by dmonteir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,10 @@
 
 void *ft_memmove(void *s1, const void *s2, size_t n)
 {
-    size_t i;
-    char *str1;
-    const char *str2;
     
-    i = 0;
+    unsigned char *str1;
+    unsigned char *str2;
+    
     str1 = (unsigned char *)s1;
     str2 = (unsigned char *)s2;
     
@@ -51,7 +50,7 @@ int main () {
    char dest[] = "antes";
    const char src[]  = "depois";
 
-   //printf("dest = %s, src = %s\n", dest, src);
+   printf("dest = %s, src = %s\n", dest, src);
    ft_memmove(dest, src, 6);
    printf("dest = %s, src = %s\n", dest, src);
 
