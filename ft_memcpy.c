986c15e79414694a149a784cce9b3cccdb11bbc6
Daniela Monteiro <danimdf360@gmail.com>
2021-05-29 01:07:16 -0300

    Correçao memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: dmonteir <dmonteir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/22 12:59:19 by dmonteir          #+#    #+#             */
-/*   Updated: 2021/05/29 00:09:07 by dmonteir         ###   ########.fr       */
+/*   Updated: 2021/05/29 01:05:20 by dmonteir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,12 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	s2 = (unsigned char *)src;
 	if (!s1 && !s2)
 		return (0);
-	if (i == n)
-		return (dest);
-	while (n > 0)
+	if (s1 == s2)
+		return (s1);
+	while (i < n)
 	{
-		*s1 = *s2;
+		s1[i] = s2[i];
 		i++;
-		n--;
 	}
-	return (s1);
+	return (dest);
 }
