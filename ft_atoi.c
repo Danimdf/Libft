c6be870e59594aca2a8ec07bae938b121c2efa01
Daniela Monteiro <danimdf360@gmail.com>
2021-05-31 13:35:06 -0300

    Ajustes norminette

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,40 +6,40 @@
 /*   By: dmonteir <dmonteir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/26 17:24:55 by dmonteir          #+#    #+#             */
-/*   Updated: 2021/05/31 13:28:56 by dmonteir         ###   ########.fr       */
+/*   Updated: 2021/05/31 13:34:27 by dmonteir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int    ft_isspace(int c)
+static int	ft_isspace(int c)
 {
-    if (c == ' ' || c == '\n' || c == '\t' || c == '\r'
-        || c == '\f' || c == '\v')
-        return (1);
-    return (0);
+	if (c == ' ' || c == '\n' || c == '\t' || c == '\r'
+		|| c == '\f' || c == '\v')
+		return (1);
+	return (0);
 }
 
-int    ft_atoi(const char *nptr)
+int	ft_atoi(const char *nptr)
 {
-    int    num;
-    int    sign;
+	int	num;
+	int	sign;
 
-    while ((ft_isspace(*nptr)))
-        nptr++;
-    sign = 1;
-    if (*nptr == '+' || *nptr == '-')
-    {
-        if (*nptr == '-')
-            sign = -1;
-        nptr++;
-    }
-    num = 0;
-    while ((ft_isdigit(*nptr)))
-    {
-        num *= 10;
-        num += sign * (*nptr - '0');
-        nptr++;
-    }
-    return (num);
+	while ((ft_isspace(*nptr)))
+		nptr++;
+	sign = 1;
+	if (*nptr == '+' || *nptr == '-')
+	{
+		if (*nptr == '-')
+			sign = -1;
+		nptr++;
+	}
+	num = 0;
+	while ((ft_isdigit(*nptr)))
+	{
+		num *= 10;
+		num += sign * (*nptr - '0');
+		nptr++;
+	}
+	return (num);
 }
