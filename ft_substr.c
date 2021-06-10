a6c2c1320a04658c9e178fd8ea22e567d3c740c9
Daniela Monteiro <danimdf360@gmail.com>
2021-06-10 12:24:26 -0300

    Att norminette and functions

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: dmonteir <dmonteir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/27 18:16:43 by dmonteir          #+#    #+#             */
-/*   Updated: 2021/05/28 23:34:07 by dmonteir         ###   ########.fr       */
+/*   Updated: 2021/06/10 12:22:48 by dmonteir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,15 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	size_t	i;
-	size_t	j;
 	char	*sub;
 
-	i = 0;
-	j = 0;
 	if (!s)
 		return (NULL);
+	if (ft_strlen(s) < start)
+		return (ft_strdup(""));
 	sub = (char *)malloc(sizeof(char) * (len + 1));
 	if (!(sub))
 		return (NULL);
-	while (s[i])
-	{
-		if (i >= start && j < len)
-		{
-			sub[j] = s[i];
-			j++;
-		}
-		i++;
-	}
-	sub[j] = '\0';
+	ft_strlcpy (substr, s + start, len + 1);
 	return (sub);
 }
