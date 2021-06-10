a2026333561f9511683c760b3a8f16c4d62607cc
Daniela Monteiro <danimdf360@gmail.com>
2021-06-10 12:05:01 -0300

    att modificaçao

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: dmonteir <dmonteir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/27 11:39:59 by dmonteir          #+#    #+#             */
-/*   Updated: 2021/05/28 15:58:28 by dmonteir         ###   ########.fr       */
+/*   Updated: 2021/06/10 12:04:25 by dmonteir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,22 @@
 
 char	*ft_strdup(const char *str1)
 {
-	size_t	len;
-	char	*ptr;
-	size_t	i;
+	int i;
+	int len;
+	char *new_str;
 
-	if (!str1)
-		return (NULL);
-	len = ft_strlen(str1);
-	ptr = (char *)malloc(len + 1);
-	if (ptr == NULL)
+	len = 0;
+	while (str1[len])
+		len++;
+	new_str = (char *)malloc(sizeof(char) * (len + 1));
+	if (!(new_str))
 		return (NULL);
 	i = 0;
-	while (i < len)
+	while (str1[i])
 	{
-		ptr[i] = str1[i];
+		new_str[i] = str1[i];
 		i++;
 	}
-	ptr[len] = '\0';
-	return (ptr);
+	new_str[i] = '\0';
+	return (new_str);
 }
